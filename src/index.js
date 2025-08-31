75460b373a3d6fd848fb92514ede4b2e43c501b0
LikhithaMallipeddi <159543104+LikhithaMallipeddi@users.noreply.github.com>
2025-08-31 20:54:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const approot = ReactDOM.createRoot(document.getElementById('approot'));
 root.render(
   <React.StrictMode>
       <App />
